697bdce80a085f6bbd71999a9679cab472d923b8
lanavar <lanavar@miners.utep.edu>
2018-09-19 23:35:10 -0600

    Added functionality for exiting

--- shell/myshell.py
+++ shell/myshell.py
@@ -2,11 +2,18 @@
 #Importing necessary
 import sys, re, os, subprocess, time
 
+# Initial prompt
 function = input(">>$ ")
+done = False
+class ExitClause(Exception):
+    pass
 
 # Loop for the prompt
-while(True):
+while(not done):
     try:
+        #handling exit typed in
+        if function == "exit()":
+            raise ExitClause("Exiting bye")
         pid = os.getpid()        
         print("Function entered is " + function)
         os.write(1, ("About to fork (pid:%d)\n" % pid).encode())
@@ -18,6 +25,7 @@ while(True):
         elif rc == 0:
             os.write(1, ("I am child. My pid==%d. Parent's pid=%d. Function is %s\n" % (os.getpid(), pid, function)).encode())
             args = function.split()
+            # handle event that exit is entered
             for dir in re.split(":", os.environ['PATH']):
                 program = "%s/%s" % (dir, args[0])
                 os.write(1, ("Child:  ...trying to exec %s\n" % program).encode())
@@ -36,5 +44,9 @@ while(True):
     except EOFError:
         print("")
         print("Done")
+        break
+
+    except ExitClause:
+        print("Exiting ... Bye")
         break
 
